bbd6a4732528cb309ea08913e511a59745b3b654
mod0dethink <tanakaryuto1994@gmail.com>
2024-10-31 14:08:01 +0900

    EloRatingの実装

---
+++ backend/api/rate/model.go
@@ -0,0 +1,18 @@
+package rate
+
+type RatingRequest struct {
+	WinnerID string `json:"winner_id"`
+	LoserID  string `json:"loser_id"`
+	GameType string `json:"game_type"` // ここは今のところ"quiz"固定
+}
+
+type RatingResponse struct {
+	WinnerNewRating int `json:"winner_new_rating"`
+	LoserNewRating  int `json:"loser_new_rating"`
+	RatingChange    int `json:"rating_change"`
+}
+
+type PlayerRating struct {
+	Username string `json:"username"`
+	Rating   int    `json:"rating"`
+}
