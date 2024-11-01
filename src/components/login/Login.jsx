a28d899cbadaa78a2393aad1299295dc5828f11a
2230142 <2230142@ecc.ac.jp>
2024-11-01 13:45:37 +0900

    style:ログイン必要要素の配置（仮）

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,9 +3,17 @@ import React from 'react'
 const Login = () => {
   return (
     <>
-        <div className="mx-auto my-auto">
-
-        </div>
+      <div className="m-auto border-4 rounded-2xl">
+        <form action="/" method='POST' className="flex flex-col">
+          <p>プレイヤーID</p>
+          <input type="text" placeholder="プレイヤーIDを入力" className="border-4 rounded-2xl"></input>
+          <p>パスワード</p>
+          <input type="password" placeholder="パスワードを入力" className="border-4 rounded-2xl"></input>
+          <p>確認用パスワード</p>
+          <input type="password" placeholder="確認用パスワードを入力" className="border-4 rounded-2xl"></input>
+          <button type="submit" className="text-red-500 border-4 rounded-2xl my-5">ログイン</button>
+        </form>
+      </div>
     </>
   )
 }
