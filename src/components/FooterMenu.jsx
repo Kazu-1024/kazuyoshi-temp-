ff913205481782fcc48026ee12743839afe29efa
2230142 <2230142@ecc.ac.jp>
2024-11-01 13:46:16 +0900

    style:修正

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -12,7 +12,7 @@ const FooterMenu = () => {
       <button className="border-4" onClick={() => navigate('/CreateQuestions')}>作問</button>
       <button className="border-4" onClick={() => navigate('/QuizReview')}>復習</button>
       <button className="border-4" onClick={handleGoHome}>ホーム</button>
-      <button className="border-4" onClick={() => navigate('/Rankings')}>ランキング</button>
+      <button className="border-4" onClick={() => navigate('/Rankings')}>ランク</button>
       <button className="border-4" onClick={() => navigate('/UserDetails')}>ユーザー</button>
     </>
   )
