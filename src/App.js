00c51de03b6b69740203427ba553a64d3113e288
2230142 <2230142@ecc.ac.jp>
2024-11-01 13:46:49 +0900

    feat: routeの追加、スタイルの修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home'
+import Login from './components/login/Login';
+import Home from './components/Home';
 import Rankings from './components/Rankings';
+import QuizReview from "./components/QuizReview";
 import UserDetails from './components/UserDetails';
 import CreateQuestions from "./components/CreateQuestions";
 import CreateQuestions_details from "./components/CreateQuestions_details";
@@ -10,14 +12,16 @@ import FooterMenu from './components/FooterMenu';
 function App() {
   return (
     <Router>
-      <div>
+      <>
+      <div className="flex flex-col h-screen">
         <header className="text-center py-4 bg-gray-800 text-white">
-          <h1 className="text-2xl font-bold">Real-time Battle App</h1>
+          <h1 className="text-2xl font-bold">Luxon</h1>
         </header>
-        <main className="p-4">
+        <main className="flex-1 p-4">
           <Routes>
+            <Route path="/Login" element={<Login />} />
             <Route path="/rankings" element={<Rankings />} />
-            <Route path="/rankings" element={<Rankings />} />
+            <Route path="/QuizReview" element={<QuizReview />} />
             <Route path="/" element={<Home />} />
             <Route path="/rankings" element={<Rankings />} />            
             <Route path="/UserDetails" element={<UserDetails />} />
@@ -25,8 +29,11 @@ function App() {
             <Route path="/CreateQuestions_details" element={<CreateQuestions_details />} />
           </Routes>
         </main>
-        <FooterMenu />
-      </div>
+        <footer className="bg-cyan-400 absolute bottom-0 flex justify-around w-full py-5">
+          <FooterMenu />
+        </footer>
+        </div>
+      </>
     </Router>
   );
 }
