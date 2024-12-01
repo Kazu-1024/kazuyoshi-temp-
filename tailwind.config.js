1637194d8a520ff2f480073baaeecdc320c73db3
MSI\2230169 <2230169@ecc.ac.jp>
2024-12-01 22:31:04 +0900

    12/01家に転送

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,22 @@ module.exports = {
       customShadow: {
         'shadowTop': "0 -4px 6px rgba(0, 0, 0, 0.1)",
       },
-      hatchedBox:{
-        'hatchedBox':'linear-gradient(-25deg, pink 50%, skyblue 0);'
-
-      },
+      borderImage: {
+        'gradient': 'linear-gradient(to right, #4b5563, #6b7280) 1'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+  function({ addUtilities }) {
+    const newUtilities = {
+      '.border-gradient-gray-black': {
+        'border-image-source': 'linear-gradient(to right, gray 0%, black 100%)',
+        'border-image-slice': '1',
+        'border-width': '4px',
+        'border-style': 'solid',
+      }
+    }
+    addUtilities(newUtilities)
+  }],
 }
 
