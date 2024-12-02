6853e0e5a317e7cd266bae2feeae50f2b8e16e84
2230142 <2230142@ecc.ac.jp>
2024-12-02 09:43:36 +0900

    bgImageの変更、フォントファミリーの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,19 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-background': "url('../src/assets/images/BackGround.png')",
+        'custom-background': "url('../src/assets/images/KazuyoshiSuperNova.png')",
       },
       customShadow: {
         'shadowTop': "0 -4px 6px rgba(0, 0, 0, 0.1)",
       },
+      fontFamily: {
+        iceland: ['Iceland', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        kdam: ['"Kdam Thmor Pro"', 'sans-serif'], 
+        notoSansJp: ['"Noto Sans JP"', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
+        afacad: ['afacad', 'sans-serif'],
+      }
     },
   },
   plugins: [],
