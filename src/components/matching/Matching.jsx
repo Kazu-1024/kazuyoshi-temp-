b46858538ff9cef7b45a0cb15a814c757bdff57c
2230142 <2230142@ecc.ac.jp>
2024-12-02 10:56:13 +0900

    edit: headerの追記

--- src/components/matching/Matching.jsx
+++ src/components/matching/Matching.jsx
@@ -4,6 +4,7 @@ import secHeader from "../../assets/images/Group_83.png";
 import cancelButton from "../../assets/images/cancel_button.png";
 import MTH from "../../assets/images/MTH.png";
 import RatingB from "../../assets/images/Frame_37.png";
+import Tag1on1 from "../../assets/images/1on1_tag.png";
 
 const Matching = () => {
   // アクティブなスライドのインデックスを管理するstate
@@ -65,6 +66,12 @@ const Matching = () => {
   //-------------------------------------------------------------------------
   return (
     <>
+      <div className="relative h-32 border-b-2 bg-white border-black shadow-md flex flex-col justify-center items-center">
+        <div className="w-5/6 border-t border-2 border-gray-300 mt-2" />
+        <p className="font-notoSansJp font-bold text-4xl my-3">英検準一級</p>
+        <div className="w-5/6 border-t border-2 border-gray-300" />
+        <img src={Tag1on1} className="absolute top-2 left-2" />
+      </div>
       <div className="mt-16 flex justify-center" aria-label="読み込み中">
         {/*自分のアイコン、ランク、レート表示 */}
         <div className="mr-24">
