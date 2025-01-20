e9b33c39e5218bee1a559f39ee41c42afdf782fe
MSI\2230169 <2230169@ecc.ac.jp>
2025-01-20 10:28:58 +0900

    ハンバーガーメニュー修正

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,11 +7,11 @@ const Header = () => {
   const [rate, setRate] = useState(1500)
   return (
     <>
+      <div className='absolute inset-0 flex items-center justify-center font-jaro text-3xl'>HOME</div>
       <div className="flex items-center">
         <HamburgerMenu />
         <Notifications />
       </div>
-      <div className='absolute inset-0 flex items-center justify-center font-jaro text-3xl'>HOME</div>
       <img src={MTH} alt="" className="rounded-full h-12 w-12 ml-auto mr-5" />
     </>
   )
