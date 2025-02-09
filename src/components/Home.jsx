01415fa02577f1777f796dcab400fdb55d849277
2230142 <2230142@ecc.ac.jp>
2025-02-10 03:31:00 +0900

    レーティング取得

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ import ratingBg from '../assets/images/ratingBg.png';
 import HelpCircle from '../assets/images/HelpCircle.png';
 import ranking from '../assets/images/ranking.png';
 import Dropdown from './common/Dropdown';
+import PlayerRating from './common/PlayerRating';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -49,21 +50,23 @@ const Home = () => {
   return (
     <div className="relative flex flex-col h-full w-full">
       {/* レーティング部分 */}
-      <div className="absolute right-1 top-1">
+      {/* <div className="absolute right-1 top-1">
         <img src={ratingBg} alt="Rating Background" className="block" />
         <p className="absolute inset-0 left-9 flex items-center text-black font-jaro tracking-wider text-xl outlined-bold">
           {rate}
         </p>
-      </div>
+      </div> */}
+
+      <PlayerRating username="ryuya" position="home"/>
 
       {/* プルダウンコンポーネントを呼び出し */}
       <Dropdown selectedOption={selectedOption} setOptionSelect={handleOptionSelect} />
 
       {/* 画像とボタン */}
       <div className="relative flex flex-col items-center justify-center aspect-square w-9/12 max-h-96 mx-auto mt-5">
-        <img src={selectStyle} className="w-full h-full object-contain" alt="Style Select" />
+        <img src={selectStyle} role="presentation" draggable="false" className="w-full h-full object-contain" alt="Style Select" />
         <button className="absolute bottom-[8%] transform -translate-y-1/2 w-1/2 h-[20px] flex items-center justify-center aspect-[5/1]" onClick={handleImageClick}>
-          <img src={startButton} alt="Start Button" className="object-cover" />
+          <img src={startButton} role="presentation" draggable="false" alt="Start Button" className="object-cover" />
         </button>
       </div>
 
@@ -72,7 +75,7 @@ const Home = () => {
         <button onClick={() => {
           console.log('ヘルプボタンがクリックされました');
         }}>
-          <img src={HelpCircle} className="w-10 h-10" alt="Help" />
+          <img src={HelpCircle} role="presentation" draggable="false" className="w-10 h-10" alt="Help" />
         </button>
       </div>
       {/*ランキングボタン */}
@@ -81,7 +84,7 @@ const Home = () => {
           rankingpage();
           console.log('ランキングボタンがクリックされました');
         }}>
-          <img src={ranking} className="w-10 h-10" alt="Help" />
+          <img src={ranking} role="presentation" draggable="false" className="w-10 h-10" alt="Help" />
         </button>
       </div>
 
